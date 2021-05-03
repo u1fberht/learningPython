7943f69548bb354198a33adcc2b6c3fd940339fd
Ulfberht <32742678+CodyConroy@users.noreply.github.com>
2021-05-02 23:10:04 -0400

    Changing player_age to int variable and concatinating into print statement

--- messingAround.py
+++ messingAround.py
@@ -1,6 +1,5 @@
 print("Hello World")
 
-
 print("\n    /|")
 print("   / |")
 print("  /  |")
@@ -9,8 +8,8 @@ print("/____| \n")
 
 
 player_name = "cody"
-player_age = "21"
+player_age = 21
 is_male = True
 print(player_name.capitalize() + " enjoys going for long walks on the beach.")
-print(player_name.capitalize() + " has enjoyed this since he was " + player_age +  " years old")
+print(player_name.capitalize() + " has enjoyed this since he was " + str(player_age) +  " years old")
 print(player_name[0].upper() + " is the first letter of " + player_name.capitalize() + "'s name")
