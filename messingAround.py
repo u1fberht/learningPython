682027a7d15ae55e7dc9e3d90958f78da82c5f4a
Ulfberht <32742678+CodyConroy@users.noreply.github.com>
2021-05-02 22:38:29 -0400

    Just messing around

---
+++ messingAround.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+
+print("\n    /|")
+print("   / |")
+print("  /  |")
+print(" /   |")
+print("/____| \n")
+
+
+player_name = "cody"
+player_age = "21"
+is_male = True
+print(player_name.capitalize() + " enjoys going for long walks on the beach.")
+print(player_name.capitalize() + " has enjoyed this since he was " + player_age +  " years old")
+print(player_name[0].upper() + " is the first letter of " + player_name.capitalize() + "'s name")
