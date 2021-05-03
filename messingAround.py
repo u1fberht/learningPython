6ce878a459d8993500c56f9ec71280eccd3bfb48
Ulfberht <32742678+CodyConroy@users.noreply.github.com>
2021-05-02 23:27:27 -0400

    Taking in user input

--- messingAround.py
+++ messingAround.py
@@ -13,3 +13,8 @@ is_male = True
 print(player_name.capitalize() + " enjoys going for long walks on the beach.")
 print(player_name.capitalize() + " has enjoyed this since he was " + str(player_age) +  " years old")
 print(player_name[0].upper() + " is the first letter of " + player_name.capitalize() + "'s name")
+
+# Taking user age as a variable
+
+user_age = input("Enter your age: ")
+print(player_name.capitalize() + " wishes he was " + str(user_age))
