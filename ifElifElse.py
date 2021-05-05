6ebe39f60125e252c6d76f24d70b39b800665624
Ulfberht <32742678+CodyConroy@users.noreply.github.com>
2021-05-05 17:39:44 -0400

    Integrating if, elif into python for loops

--- ifElifElse.py
+++ ifElifElse.py
@@ -6,4 +6,27 @@ elif favorite_color.lower() == cody_favorite_color.lower() :
     print("Hey! " + favorite_color + " is my favorite color too!\nAwesome stuff!")
 else :
     print("I think " + favorite_color + " is a cool color. I, however, prefer " + cody_favorite_color.lower())
-    
+
+# Messing with more string functions
+
+print("When I was younger, my favorite color was " + cody_favorite_color.replace("Purple", "Orange"))
+print("This was because Commander Cody from Attack of the Clones had " + cody_favorite_color.replace("Purple", "Orange") + " accents")
+
+# Seeing if user inputted number is even using a for loop
+
+user_num = input("Enter a number: ")
+for x in range(0, 4) :
+    if user_num.isdigit() == 0 :
+        user_num = input("Please enter a valid number: ")
+    elif user_num.isdigit() == 1 :
+        user_num = int(user_num)
+        if user_num % 2 == 0 :
+            print("Congratulations! You have entered an even number!")
+            break
+        else :
+            print("Congratulations! You have entered either an odd number or a number with a decimal!")
+            break
+
+user_num = str(user_num)
+if user_num.isdigit() == 0 :
+        print("You have failed to enter a valid number: congratulations. Your parents have failed.")
